f78685b287074f6d70df263d1ff78338c4c0056a
Anton Polchenko <Kreaitor@yandex.ru>
2019-08-15 17:51:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterwiewConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fibonacci sequence realization
+
+            Console.WriteLine("Where to stop cointing");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int first = 1;
+            Console.Write("{0} ", first);
+            int second = 1;
+            Console.Write("{0} ", second);
+            int sum = 0;
+
+            while (number >= sum)
+            {
+                sum = first + second;
+
+                Console.Write("{0} ", sum);
+
+                first = second;
+                second = sum;
+            }
+
+            Console.WriteLine("End, Press any key");
+            Console.ReadKey();
+        }
+    }
+}
